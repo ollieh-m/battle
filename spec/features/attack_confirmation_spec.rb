0b8378b24799a6d8d7fc31cb97fb0bd255a88ae0
naridas <gregsparta@gmail.com>
2016-04-28 18:06:35 +0100

    working on part 28

--- spec/features/attack_confirmation_spec.rb
+++ spec/features/attack_confirmation_spec.rb
@@ -2,15 +2,20 @@
 feature "Attack confirmation" do
   scenario "Player 1 gets confirmation when they attack Player 2" do
     sign_in_and_play
-    click_link "Attack!"
+    click_link 'Attack'
     expect(page).to have_content("Amy attacked Bizarro Amy!")
   end
 
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
-    click_link 'Attack'
-    click_link 'OK'
-    expect(page).not_to have_content 'Bizarro Amy has 100 hit points'
+    attack_confirmation
     expect(page).to have_content 'Bizarro Amy has 90 hit points'
+  end
+
+  scenario 'reduce Player 1 HP by 10' do
+    sign_in_and_play
+    attack_confirmation
+    attack_confirmation
+    expect(page).to have_content 'Amy has 90 hit points'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,7 +1,8 @@
 
 feature "Hit points" do
-  scenario "Show Player 2's hit point" do
+  scenario "Show Players hit point" do
     sign_in_and_play
-    expect(page).to have_text("Bizarro Amy has 100 hit points. Prepare for BATTLE!")
+    expect(page).to have_text("Bizarro Amy has 100 hit points")
+    expect(page).to have_text("Amy has 100 hit points")
   end
 end
